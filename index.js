d01a7b95540585911fa65115c8e451bd80421cec
olalekan <olalekanotuyiga@gmail.com>
2025-10-26 20:24:41 +0100

    initialization successfull

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ async function environmentSetup() {
   const myAccountId = process.env.MY_ACCOUNT_ID;
   const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
+  // create hedera client for testnet
+  const client = Client.forTestnet();
+
+  // Set the client's operator using your account ID and private key
+  client.setOperator(myAccountId, myPrivateKey);
+
+  // set the default maximun transactio fee(in Hbar) for all transactions
+  client.setDefaultMaxTransactionFee(new Hbar(100));
+
+  // set the maximun query payment(in Hbar) for all queries
+  client.setDefaultMaxQueryPayment(new Hbar(50));
+
   // If we weren't able to grab it, we should throw a new error
   if (!myAccountId || !myPrivateKey) {
     throw new Error(
